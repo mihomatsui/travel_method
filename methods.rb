4b30307d8717337c3fc8bd8a25016772dbce8703
mihomatsui <mihomatsui.24@gmail.com>
2020-12-19 11:20:32 +0900

    メソッド部分のみに修正

--- methods.rb
+++ methods.rb
@@ -1,6 +1,3 @@
-
-# メソッドを定義する部分 --------------------------
-
 # プランを表示する
 def disp_plans(plans)
   puts "旅行プランを選択してください。"
@@ -44,15 +41,3 @@ def calculate_charges(chosen_plan, number_of_people)
 end
 
 # メソッドを呼び出す部分 ----------------------
-
-# 旅行プランデータ
-plans = [
-  {place: "沖縄旅行", price: 10000},
-  {place: "北海道旅行", price: 20000},
-  {place: "九州旅行", price: 15000},
-]
-
-disp_plans(plans) # プランを表示する
-chosen_plan = choose_plan(plans) # プランを選択する
-number_of_people = decide_number_of_people(chosen_plan) # 人数を決定する
-calculate_charges(chosen_plan, number_of_people) # 合計金額を計算する
